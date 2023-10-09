8ca33c90a1eb7af0d2eea4db6371bd8845bb2938
Tychiquekadi2 <tychiquemukadi@gmail.com>
2023-10-08 23:32:31 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+	while (a <= 99)
+	{
+		int b = a + 1;
+		while (j <= 99)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a != 98 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
