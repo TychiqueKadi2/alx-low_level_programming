a5e3e4d0e2df900bb117b9c14b751a3f0cbf6b70
Tychiquekadi2 <tychiquemukadi@gmail.com>
2023-10-10 08:04:47 -0700

    _putcahr

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - prints lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+
+	_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints x10 lowercase alphabets
+ */
+void print_alphabet_x10(void);
+
