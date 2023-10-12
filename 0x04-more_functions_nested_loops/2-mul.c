f79a1ba62d11295db834344f843ab49b2f72dca1
Tychiquekadi2 <tychiquemukadi@gmail.com>
2023-10-12 03:01:32 -0700

    more nested lopps task 0 to 5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiples two intergers
+ * @a: varible to multiple
+ * @b: varible to multiple
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints n 0 to 9
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
