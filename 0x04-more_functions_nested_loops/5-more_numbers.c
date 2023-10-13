d132b39b1dab6a4fc15744289859b7befc18b9a1
Tychiquekadi2 <tychiquemukadi@gmail.com>
2023-10-12 23:58:28 -0700

    corrections more nested

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,21 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	while (a <= 9)
 	{
-		for (b = 0; b < 15; b++)
+		b = 0;
+
+		while (b <= 14)
 		{
-			if (b >= 10)
+			if (b > 9)
 			{
-				_putchar(b / 10 + 48);
-				_putchar(b % 10 + 48);
+				_putchar(b / 10 + '0');
 			}
+			_putchar(b % 10 + '0');
+			b++;
 		}
+
 		_putchar('\n');
+		a++;
 	}
 }
