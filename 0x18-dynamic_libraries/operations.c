adc6fc83e4a00580cd39bfe13d2ae5cb3533c143
TychiqueKadi2 <tychiquemukadi@gmail.com>
2024-01-15 06:19:29 -0800

    a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds two intergers
+ * @x: first int
+ * @y: seconf int
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - subyracts two intergers
+ * @x: first int
+ * @y: seconf int
+ * Return: difference
+ */
+int sub(int x, int y)
+{
+        return (x - y);
+}
+
+/**
+ * mod - mod of two intergers
+ * @x: first int
+ * @y: seconf int
+ * Return: mod
+ */
+int mod(int x, int y)
+{
+        return (x % y);
+}
+
+/**
+ * mul - mulutiplies two intergers
+ * @x: first int
+ * @y: seconf int
+ * Return: multiplication
+ */
+int mul(int x, int y)
+{
+        return (x * y);
+}
+
+/**
+ * div - divides two intergers
+ * @x: first int
+ * @y: seconf int
+ * Return: division
+ */
+int div(int x, int y)
+{
+        return (x / y);
+}
